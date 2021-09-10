077e0b9b8356a7b70da6f4b6d05cea25774fd5e7
gem-nidhigupta <nidhi.gupta@geminisolutions.in>
2021-09-10 15:24:25 +0530

    convert image to base 64

--- src/app/get-user-details-page/get-user-details-page.component.ts
+++ src/app/get-user-details-page/get-user-details-page.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataServiceService } from 'src/services/data-service.service';
+import { HelperServiceService } from 'src/services/helper-service.service';
 @Component({
   selector: 'app-get-user-details-page',
   templateUrl: './get-user-details-page.component.html',
@@ -18,14 +19,18 @@ export class GetUserDetailsPageComponent implements OnInit {
   userDetails!: FormGroup;
   preExistingDataArray: any = [];
   counter = 0;
+  urlbase64: any;
+  thumbnail_urlbase64: any;
   detailsToBeSent = {
     title: '',
     url: '',
     thumb_url: '',
   };
 
+  getImagesFromAssets: any = [];
   constructor(
     private dataService: DataServiceService,
+    private helperService: HelperServiceService,
     private router: Router
   ) {}
 
@@ -39,8 +44,8 @@ export class GetUserDetailsPageComponent implements OnInit {
   onSubmit() {
     console.log('data that is sent', this.userDetails.value);
     this.detailsToBeSent.title = this.userDetails.value?.title;
-    this.detailsToBeSent.url = this.userDetails.value?.url;
-    this.detailsToBeSent.thumb_url = this.userDetails.value?.thumb_url;
+    this.detailsToBeSent.url = this.urlbase64;
+    this.detailsToBeSent.thumb_url = this.thumbnail_urlbase64;
     this.dataService.sendData(this.detailsToBeSent).subscribe(
       (res) => {
         console.log('response from api', res);
@@ -69,7 +74,16 @@ export class GetUserDetailsPageComponent implements OnInit {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
       reader.onload = (res: any) => {
-        this.userDetails.value.thumb_url = reader.result;
+        this.thumbnail_urlbase64 = reader.result;
+      };
+    }
+  }
+  onFileChangeOfUrl(event: any) {
+    if (event.target.files[0] && event.target.files[0].length != 0) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (res: any) => {
+        this.urlbase64 = reader.result;
       };
     }
   }
---
+++ src/services/helper-service.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root',
+})
+export class HelperServiceService {
+  constructor(private http: HttpClient) {}
+}
